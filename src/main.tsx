8dc0605cdf4203214c668e56df7eb75e03b4979d
abdukarimov.h2 <abdukarimov.h2@wildberries.work>
2023-10-07 21:10:48 +0300

    feat: change app color

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./app";
+import "@/shared/assets/styles/main.scss"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
